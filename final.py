ded8c47a23f3538811f4fd7c6d9353e399877356
epelayoc <97033678+epelayoc@users.noreply.github.com>
2022-03-29 17:24:41 +0200

    Update final.py

--- final.py
+++ final.py
@@ -3,59 +3,35 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
-
-database = 'ecsel_database.db'
-
-selects = {
-'country': 
-''' SELECT Acronym FROM countries WHERE Country = "{}" ''',
- 
-'grants': 
-''' SELECT SUM(o.ecContribution) AS grants 
-    FROM organizations o JOIN projects p ON o.projectID==p.projectID
-    WHERE o.country = '{}'
-    GROUP BY p.year ''',
-    
-'participants':  
-''' SELECT shortName, name, activityType, organizationURL, COUNT(ecContribution) n_projects, SUM(ecContribution) grants 
-    FROM organizations 
-    WHERE country =  '{}' 
-    GROUP BY name ORDER BY SUM(ecContribution) DESC ''',
-    
-'coordinators': 
-''' SELECT o.shortName, o.name, p.acronym, p.keywords 
-    FROM organizations o JOIN projects p ON o.projectID = p.projectID 
-    WHERE o.country = '{}' AND o.role ='coordinator' '''
-}
+# colnames= {c:c for c in list(df)}
 
 # Title
 image = Image.open('KDT-JU.png')
-
 st.image(image)
 st.title("Partner search tool")
 
 # Select country
-conn = sqlite3.connect(database)
-ct = st.selectbox('Select country',
-     ['Spain', 'France', 'Germany'])
-country = pd.read_sql(selects['country'].format(ct), conn)
-country = country.Acronym.item()
-st.write(f'You selected: {country}-{ct}')
+countries = ['ES', 'FR', 'DE']
+ct = {'ES': 'Spain', 'DE': 'Germany', 'FR':'France'}
+country = st.selectbox('Select country',countries)
+st.write(f'You selected: {country}-{ct[country]}')
 
-# Other selects
-dfs = {}
-for key,sel in selects.items():
-    dfs[key]=pd.read_sql(sel.format(country), conn)  
+# SQL queries
+conn = sqlite3.connect('mockDB.db')
+df_grants = pd.read_sql(f"SELECT year, grants FROM grants WHERE country = '{country}' ", conn)
+df_grants = df_grants.set_index('year')
+df_participants = pd.read_sql(f"SELECT * FROM participants WHERE country = '{country}' ", conn)
+df_coordinators = pd.read_sql(f"SELECT * FROM coordinators WHERE country = '{country}' ", conn)
 conn.close()
 
 # grants
-st.subheader(f'Yearly EC contribution in {ct} (€)')
-st.bar_chart(dfs['grants'])
+st.subheader(f'Yearly EC contribution in {ct[country]} (€)')
+st.bar_chart(df_grants)
 
 # participants
-st.subheader(f'Participants in {ct}')
-st.dataframe(dfs['participants'])
-csv_p = dfs['participants'].to_csv().encode('utf-8')
+st.subheader(f'Participants in {ct[country]}')
+st.dataframe(df_participants)
+csv_p = df_participants.to_csv().encode('utf-8')
 st.download_button(
      label="Download participants data as CSV",
      data=csv_p,
@@ -64,9 +40,9 @@ st.download_button(
  )
 
 # coordinators
-st.subheader(f'Project cooordinators in {ct}')
-st.dataframe(dfs['coordinators'])
-csv_c = dfs['coordinators'].to_csv().encode('utf-8')
+st.subheader(f'Project cooordinators in {ct[country]}')
+st.dataframe(df_coordinators)
+csv_c = df_coordinators.to_csv().encode('utf-8')
 st.download_button(
      label="Download coordinators data as CSV",
      data=csv_c,
